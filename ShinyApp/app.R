82310224f827c289f3e710ea67b76c1ac58e846b
AnqiW <waq1040593295@outlook.com>
2025-06-28 17:17:04 +0800

    delete test

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -10,8 +10,7 @@ library(tidyr)
 library(plotly)
 library(DT)
 
-# wanganqi Load data
-#yyh
+# Load data
 kg <- fromJSON("data/MC1_graph.json")
 nodes_tbl <- as_tibble(kg$nodes)
 edges_tbl <- as_tibble(kg$links)
